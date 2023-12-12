da9c5d9feeba9d84ec2d1c3b13e98d4d22985cb4
ColoringHydra <guusl@outlook.com>
2023-12-12 14:33:50 +0100

    project aangemaakt

---
+++ Debugy/Debugy/main.c
@@ -0,0 +1,18 @@
+/*
+ * Debugy.c
+ *
+ * Created: 12-12-2023 14:32:06
+ * Author : guusl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
